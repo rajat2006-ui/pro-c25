ca5702d7306b9a635772f7c82f5db28c1752cb05
rajat2006-ui <73226257+rajat2006-ui@users.noreply.github.com>
2020-11-06 17:30:39 +0530

    Delete box.js

--- box.js
+++
@@ -1,7 +0,0 @@
-class Box extends BaseClass{
-        constructor(x,y,width,height){
-        var option={density:350}
-        super(x,y,width,height,option);
-        Matter.Body.setStatic(this.body,true);
-    }
-}
